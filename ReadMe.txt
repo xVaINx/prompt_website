add5967dd8acee17526a83dd1dfa9da79d39ffc6
xVaINx <ayushsp19@gmail.com>
2023-08-06 22:08:47 +0530

    setup, home page, googleauth, db connect&fetch

--- ReadMe.txt
+++
@@ -1,3 +0,0 @@
-This project is a practice project to learn NEXT JS.
-
-Start Date : 06/08/2023
---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useState } from "react"
+
+const CreatePrompt = () => {
+  return (
+    <div>
+      Create 
+    </div>
+  )
+}
+
+export default CreatePrompt
